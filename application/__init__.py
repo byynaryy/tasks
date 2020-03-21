c18cebb37d6ada74e74b6be1f6ba8a01c27ec913
Jeff <home@jeffs-air.home>
2020-03-21 23:33:10 +0100

    add routes in a views file

--- application/__init__.py
+++ application/__init__.py
@@ -1,11 +1,13 @@
 import os
 
-from flask import Flask, render_template
-
+from flask import Flask 
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
+
+    import application.views
+
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'application.sqlite'),
@@ -24,9 +26,9 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    # a simple page that says hello
-    @app.route('/')
-    def index():
-        return render_template('tasks/index.html')
+    from . import views
+    app.register_blueprint(views.bp)
+    app.add_url_rule('/', endpoint='index')
+    app.add_url_rule('/form', endpoint='')
 
     return app
